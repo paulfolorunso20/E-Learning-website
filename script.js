53d11d40154210d98c6a92be58c3c73d49b1c63e
Paul Folorunso <folorunsopaul421@gmail.com>
2024-11-17 19:23:57 +0100

    vscode

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
 const toggleNavbarVisibility = () => {
   isNavbarExpanded = !isNavbarExpanded;
   navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
-};
-
-navbarToggle.addEventListener("click", toggleNavbarVisibility);
-
+}
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
